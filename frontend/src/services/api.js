9fc16fb4504b3165ebd5c03b0d08279c991e0f5b
mohansai1732 <mohansai1732@gmail.com>
2026-03-10 11:25:23 +0530

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://web-chat-application-server.onrender.com',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
