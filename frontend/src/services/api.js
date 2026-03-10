bfe3ab05fdff843aae302d23aeed8c33903bb028
mohansai1732 <mohansai1732@gmail.com>
2026-03-10 11:31:09 +0530

    Update API base URL to include '/api' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://web-chat-application-server.onrender.com',
+  baseURL: 'https://web-chat-application-server.onrender.com/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
